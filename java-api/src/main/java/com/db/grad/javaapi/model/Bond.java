2e4846e47df51e232682002b655f4e89ce27e5cf
Meg <mpenaganti12@gmail.com>
2023-08-03 14:51:17 +0100

    adding fields to the bond model

--- java-api/src/main/java/com/db/grad/javaapi/model/Bond.java
+++ java-api/src/main/java/com/db/grad/javaapi/model/Bond.java
@@ -1,33 +1,131 @@
 package com.db.grad.javaapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
-
+@Entity
+@Table(name = "bonds")
 public class Bond {
+
+    @Id
+    private long id;
     private int isin;
     private int cusip;
     private String bondIssuer;
     private String bondHolder;
     private Date maturityDate;
+    private int quantity;
+    private long unitPrice;
+    private String currency;
+    private String buyOrSell;
+    private Date tradeDate;
+    private Date settlementDate;
+    private String bondType;
+    private int faceValue;
+    private long couponPercent;
+    private String bondStatus;
+    private String tradeStatus;
 
-    public Bond(int isin, int cusip, String bondIssuer, String bondHolder, Date maturityDate) {
-        this.isin = isin;
-        this.cusip = cusip;
-        this.bondIssuer = bondIssuer;
-        this.bondHolder = bondHolder;
-        this.maturityDate = maturityDate;
+    @Id
+    @Column(name = "bond_id", nullable = false)
+    public long getId() {
+        return id;
     }
 
-    @Override
-    public String toString() {
-        return "Bond{" +
-                "isin=" + isin +
-                ", cusip=" + cusip +
-                ", bondIssuer='" + bondIssuer + '\'' +
-                ", bondHolder='" + bondHolder + '\'' +
-                ", maturityDate=" + maturityDate +
-                '}';
+    public void setId(long id) {
+        this.id = id;
+    }
+    @Column(name = "quantity", nullable = false)
+    public int getQuantity() {
+        return quantity;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    @Column(name = "unit_price", nullable = false)
+    public long getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(long unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    @Column(name = "currency", nullable = false)
+    public String getCurrency() {
+        return currency;
+    }
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    @Column(name = "buy_or_sell", nullable = false)
+    public String getBuyOrSell() {
+        return buyOrSell;
+    }
+
+    public void setBuyOrSell(String buyOrSell) {
+        this.buyOrSell = buyOrSell;
+    }
+    @Column(name = "trade_date", nullable = false)
+    public Date getTradeDate() {
+        return tradeDate;
+    }
+
+    public void setTradeDate(Date tradeDate) {
+        this.tradeDate = tradeDate;
+    }
+    @Column(name = "settlement_date", nullable = false)
+    public Date getSettlementDate() {
+        return settlementDate;
+    }
+
+    public void setSettlementDate(Date settlementDate) {
+        this.settlementDate = settlementDate;
+    }
+    @Column(name = "bond_type", nullable = false)
+    public String getBondType() {
+        return bondType;
+    }
+
+    public void setBondType(String bondType) {
+        this.bondType = bondType;
+    }
+    @Column(name = "face_value", nullable = false)
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public void setFaceValue(int faceValue) {
+        this.faceValue = faceValue;
+    }
+    @Column(name = "coupon_percent", nullable = false)
+    public long getCouponPercent() {
+        return couponPercent;
+    }
+
+    public void setCouponPercent(long couponPercent) {
+        this.couponPercent = couponPercent;
+    }
+    @Column(name = "bond_status", nullable = false)
+    public String getBondStatus() {
+        return bondStatus;
+    }
+
+    public void setBond_status(String bStatus) {
+        this.bondStatus = bStatus;
+    }
+    @Column(name = "trade_status", nullable = false)
+    public String getTradeStatus() {
+        return tradeStatus;
+    }
+
+    public void setTrade_status(String tStatus) {
+        this.tradeStatus = tStatus;
+    }
+
+    @Column(name = "isin", nullable = false)
     public int getIsin() {
         return isin;
     }
@@ -35,7 +133,7 @@ public class Bond {
     public void setIsin(int isin) {
         this.isin = isin;
     }
-
+    @Column(name = "cusip", nullable = false)
     public int getCusip() {
         return cusip;
     }
@@ -43,7 +141,7 @@ public class Bond {
     public void setCusip(int cusip) {
         this.cusip = cusip;
     }
-
+    @Column(name = "bond_issuer", nullable = false)
     public String getBondIssuer() {
         return bondIssuer;
     }
@@ -51,7 +149,7 @@ public class Bond {
     public void setBondIssuer(String bondIssuer) {
         this.bondIssuer = bondIssuer;
     }
-
+    @Column(name = "bond_holder", nullable = false)
     public String getBondHolder() {
         return bondHolder;
     }
@@ -59,7 +157,7 @@ public class Bond {
     public void setBondHolder(String bondHolder) {
         this.bondHolder = bondHolder;
     }
-
+    @Column(name = "maturity_date", nullable = false)
     public Date getMaturityDate() {
         return maturityDate;
     }
--- java-api/src/main/java/com/db/grad/javaapi/repository/BondRepository.java
+++ java-api/src/main/java/com/db/grad/javaapi/repository/BondRepository.java
@@ -4,10 +4,12 @@ import com.db.grad.javaapi.model.Bond;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface BondRepository extends JpaRepository <Bond, Long>
 {
+    @Query(nativeQuery = true, value = "select * from bonds where name = :isin")
     Bond findByIsin(int isin);
 
 }
